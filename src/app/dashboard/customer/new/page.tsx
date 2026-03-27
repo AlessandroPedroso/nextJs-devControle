16fab11c1abc90aa6d2e735641103aa105f7c976
Alessandro Pedroso <alessandrospedroso@outlook.com>
2026-03-27 12:25:46 -0300

    estilizando nova entrada

--- src/app/dashboard/customer/new/page.tsx
+++ src/app/dashboard/customer/new/page.tsx
@@ -1,3 +1,13 @@
+import { Container } from "@/components/container";
+import Link from "next/link";
 export default function NewCostumer() {
-  return <h1>Página cadastro cliente</h1>;
+  return (
+    <Container>
+      <main className="flex flex-col mt-9 mb-2">
+        <div className="flex items-center gap-3">
+          <Link href="/dashboard/customer">Voltar</Link>
+        </div>
+      </main>
+    </Container>
+  );
 }
